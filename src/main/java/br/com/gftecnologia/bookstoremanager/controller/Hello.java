4570bad14a5b8838f0b268a9a4a5f9b72311780f
Glauber Fernandes <glauber.fernandes@hapvida.com.br>
2022-05-06 16:58:01 -0300

    First commit: project structure created

---
+++ src/main/java/br/com/gftecnologia/bookstoremanager/controller/Hello.java
@@ -0,0 +1,16 @@
+package br.com.gftecnologia.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Hello {
+
+    @GetMapping
+    public String hello(){
+        return "Hello World!!!";
+    }
+}
